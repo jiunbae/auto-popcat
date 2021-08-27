d49c8caf259a6fd04f99d67292758c1bbdb8f562
Jiun Bae <maytryark@gmail.com>
2021-08-27 17:29:03 +0900

    Remove unused packages
    - Add README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Auto Click Popcat.click
+
+Auto click [Popcat.click](https://popcat.click/).
+
+## Usage
+
+1. Download chromedriver from [here](https://chromedriver.chromium.org/downloads) to `./bin` directory.
+2. Install python package using `pip install -r requirements.txt`
+3. Run `python auto-cat.py`
+
+### Options
+
+- `--driver`: path to the driver.
+- `--count`: count to click.
--- auto-cat.py
+++ auto-cat.py
@@ -1,24 +1,7 @@
-import time
 import argparse
-import json
-import os
-from pathlib import Path
 
 from selenium import webdriver
-from selenium.common import exceptions as se
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
-
-class Timer:
-    def __init__(self):
-        self.time = time.time()
-
-    def end(self)\
-            -> float:
-        return time.time() - self.time
 
 
 class Handler:
